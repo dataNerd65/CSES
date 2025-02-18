9e856a4a5c9bbd37c3ccd90b9ec12d8075395845
dataNerd65 <pkiarie811@gmail.com>
2025-02-18 15:09:29 +0300

    Struggled with an equation

--- IntroductoryProblems/missingnumber.cpp
+++ IntroductoryProblems/missingnumber.cpp
@@ -4,6 +4,19 @@ int main(){
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
     
+    long n;
+    std::cin>> n;
+
+    long long expected_sum = (n * (n + 1)) / 2;
+    long long currentSum = 0;
+
+    for(int i = 0; i < n - 1; ++i){
+        int num;
+        std::cin>> num;
+        currentSum += num;
+    }
+
+    std::cout<< (expected_sum - currentSum) <<std::endl;
     
     return 0;
 }
